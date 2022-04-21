4425dd8cd00c5e90792ec294be505b51c3e1efbf
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-21 11:42:11 -0400

    commit tree d48dca8293efa22158f4cba925cd95359a12d2f7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function Home() {
   return (
@@ -10,13 +10,45 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h1>This is my component</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <Route path="/">
-      <Home />
-    </Route>
-    ;
-  )
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
